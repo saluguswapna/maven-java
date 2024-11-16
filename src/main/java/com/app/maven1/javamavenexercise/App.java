a4c2d314036308856ec00675ebd18de842238371
swapna <saluguswapna24@email.com>
2024-11-16 15:36:51 +0530

    added maven web files

---
+++ src/main/java/com/app/maven1/javamavenexercise/App.java
@@ -0,0 +1,10 @@
+package com.app.maven1.javamavenexercise;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("SALUGU SWAPNA 22BD1A055L");
+    }
+}
---
+++ src/test/java/com/app/maven1/javamavenexercise/AppTest.java
@@ -0,0 +1,19 @@
+package com.app.maven1.javamavenexercise;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
